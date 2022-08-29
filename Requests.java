2ef8a9b5436874a314d04983ecaeb401e038b3a6
Jake Jenkins <jenkinsjake04@gmail.com>
2022-08-28 21:37:27 -0400

    revert to pre-java

--- Requests.java
+++
@@ -1,8 +0,0 @@
-class Request {
-    private final String BASE_URL = "https://appalcart.ridesystems.net/Services/JSONPRelay.svc/";
-    private final String API_KEY = "8882812681";
-
-    public static makeRequest(String target, paylaod, String method = "GET") {
-        
-    }
-}
