612568313fb39621f2a42774e8e2b667cd5b3923
Jake Jenkins <jenkinsjake04@gmail.com>
2022-08-30 13:54:26 -0400

    remove comented code in Requests.js

--- js/Requests.js
+++ js/Requests.js
@@ -1,13 +1,7 @@
 class Requests {
     static BASE_URL = "https://appalcart.ridesystems.net/Services/JSONPRelay.svc/";
     static API_KEY = "8882812681";
-    // static async makeRequest(target, args) {
-    //     let params = {...{"ApiKey": this.API_KEY}, ...args};
-    //     const url = this.BASE_URL + target + "?" + new URLSearchParams(params);
-    //     //console.log(url);
-    //     const responce = await fetch(url);
-    //     return responce.json();
-    // }
+
     static async makeRequest(target, args) {
         let params = {...{"ApiKey": this.API_KEY}, ...args};
         const url = this.BASE_URL + target + "?" + new URLSearchParams(params);
